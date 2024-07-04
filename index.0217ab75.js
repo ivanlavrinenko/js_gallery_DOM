6abda421f432487807f00ef05107b739b7c27509
Ivan Lavrinenko <lavrinenkoivan51@gmail.com>
2024-07-04 16:30:11 +0200

    make build

---
+++ index.0217ab75.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.querySelector(".gallery__list").addEventListener("click",function(t){t.preventDefault(),e.src=t.target.closest(".list-item__link").href});
+//# sourceMappingURL=index.0217ab75.js.map
